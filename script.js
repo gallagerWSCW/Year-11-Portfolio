c71a9c06f36755dce2340e6fa8476ad4ceedbfda
gallagerWSCW <159972359+gallagerWSCW@users.noreply.github.com>
2025-03-08 12:13:09 +1300

    change nav

--- script.js
+++ script.js
@@ -7,10 +7,11 @@ document.getElementById("mySidenav").style.width = "0";
 document.getElementById("main").style.marginLeft = "0";
 }  
 
-function openNav() {
-document.getElementById("mySidenav").style.width = "250px";
-}
-
-function closeNav() {
-document.getElementById("mySidenav").style.width = "0";
+function toggleMenu() {
+    const sidenav = document.getElementById("mySidenav");
+    if (sidenav.style.width === "250px") {
+        closeNav();
+    } else {
+        openNav();
+    }
 }
