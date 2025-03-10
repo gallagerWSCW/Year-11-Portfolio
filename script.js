5474e3f39b20ca675526d820e7be3334a95c5bdb
gallagerWSCW <159972359+gallagerWSCW@users.noreply.github.com>
2025-03-11 01:07:51 +1300

    add a transition to navigation bar

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
+    setTimeout(function(){
+      let navItems=document.querySelectorAll('.nav-list');
+      navItems.forEach((item, index)=>{
+        item.classList.add('show');
+      });
+    }, 300);
   }
 function closeNav() {
-document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mySidenav").style.width = "0";
+  let navItems = document.querySelectorAll('.nav-list');
+  navItems.forEach((item) => {
+      item.classList.remove('show');
+  });
 }  
 
